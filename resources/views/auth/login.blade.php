f9c62a2a7bdbbeb96354dfbb2061fa9eb5889ff1
CloudiosV <cloudiosss@gmail.com>
2026-01-22 17:27:40 +0700

    style: change login design

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,38 +1,47 @@
-@extends('layouts.app')
+<!doctype html>
+<html lang="en">
+<head>
+<title>Login</title>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<link href="https://fonts.googleapis.com/css?family=Lato:300,400,700&display=swap" rel="stylesheet">
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+<link rel="stylesheet" href="{{ asset('css/login-style.css') }}">
+</head>
+<body class="img js-fullheight" style="background-image: url({{ asset('img/badminton.jpg') }});">
+	<section class="ftco-section">
+		<div class="container">
+			<div class="row justify-content-center">
+				<div class="col-md-6 text-center mb-5">
+					<h2 class="heading-section text-white">Login</h2>
+				</div>
+			</div>
+			<div class="row justify-content-center">
+				<div class="col-md-6 col-lg-4">
+					<div class="login-wrap p-0">
+                        <h3 class="mb-4 text-center">Have an account?</h3>
+                        <form action="{{ route('login') }}" class="signin-form" method="POST">
+                            @csrf
 
-@section('title', 'Admin Dashboard')
+                            <div class="form-group">
+                                <input type="text" class="form-control" placeholder="Email" name="email" required>
+                            </div>
+                            <div class="form-group">
+                                <input id="password-field" type="password" class="form-control" placeholder="Password" name="password" required>
+                                <span toggle="#password-field" class="fa fa-fw fa-eye field-icon toggle-password"></span>
+                            </div>
+                            <div class="form-group">
+                                <button type="submit" class="form-control btn btn-primary submit px-3">Sign In</button>
+                            </div>
+                        </form>
+                    </div>
+				</div>
+			</div>
+		</div>
+	</section>
 
-@section('content')
-<style>
-    body {
-        overflow: hidden;
-    }
-</style>
+    <script src="{{ asset('js/login-jquery.js') }}"></script>
+    <script src="{{ asset('js/login-script.js') }}"></script>
+</body>
+</html>
 
-    <div class="d-flex justify-content-center align-items-center vh-100">
-        <div class="card w-25 p-5">
-            <div class="my-2">
-                <h2 class="text-center">Login</h2>
-            </div>
-            <form action="{{ route('login') }}" method="POST">
-                @csrf
-
-                <div class="mb-3">
-                    <label for="email" class="form-label">Email: </label>
-                    <input type="text" id="email" name="email" class="form-control" required>
-                </div>
-                
-                <div class="mb-3">
-                    <label for="password" class="form-label">Password: </label>
-                    <input type="password" id="password" name="password" class="form-control" required>
-                </div>
-
-                <div class="d-flex justify-content-around">
-                    <button type="submit" class="btn btn-primary">Login</button>
-                    {{-- <a href="{{ route('registerForm') }}" class="btn btn-light">Register</a> --}}
-                </div>
-            </form>
-        </div>
-    </div>
-
-@endsection
