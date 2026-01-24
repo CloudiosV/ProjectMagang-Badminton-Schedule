c6f89f07922edae58529005255eb370e74e4e6a3
CloudiosV <cloudiosss@gmail.com>
2026-01-24 11:42:49 +0700

    refactor: comment unused code now

--- routes/api.php
+++ routes/api.php
@@ -6,9 +6,9 @@ use App\Http\Controllers\Api\V1\LapanganController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::post('/login', [AuthController::class, 'login']);
+// Route::post('/login', [AuthController::class, 'login']);
 
-Route::middleware('auth:sanctum')->group(function () {
+// Route::middleware('auth:sanctum')->group(function () {
     Route::post('/logout', [AuthController::class, 'logout']);
 
     Route::get('lapangan/{lapangan}', [LapanganController::class, 'show']);
@@ -16,4 +16,4 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::post('lapangan/{lapangan}/jadwal', [JadwalController::class, 'store']);
     Route::put('lapangan/{lapangan}/jadwal/{jadwal}', [JadwalController::class, 'update']);
     Route::delete('lapangan/{lapangan}/jadwal/{jadwal}', [JadwalController::class, 'destroy']);
-});
+// });
